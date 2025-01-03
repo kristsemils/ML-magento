521a1507c2c966b9c3304c2e8141955b25c700d8
kristsemils <krists.gabrielis@magebit.com>
2025-01-03 14:45:11 +0200

    KO js fixed, works as intended

--- app/design/frontend/Magebit/Learning/Magento_Catalog/web/js/qty-counter.js
+++ app/design/frontend/Magebit/Learning/Magento_Catalog/web/js/qty-counter.js
@@ -1,12 +1,50 @@
 /**
- * @copyright Copyright (c) 2024 Magebit (https://magebit.com)
+ * @copyright Copyright (c) 2024 Magebit, Ltd. (https://magebit.com/)
  * @author    Magebit <info@magebit.com>
- * @license   GNU General Public License ("GPL") v3.0
+ * @license   MIT
  */
-function subtract() {
-    document.getElementById("qty").value = Math.max(1, +document.getElementById("qty").value - 1);
-}
 
-function add() {
-    document.getElementById("qty").value = Math.min(+document.getElementById("qty").max, +document.getElementById("qty").value + 1);
-}
+'use strict';
+
+define([
+    'ko',
+    'uiComponent'
+], function (ko, Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            maxQty: 0,
+            initialQty: 1
+        },
+
+        initialize: function (config) {
+            this._super();
+            this.maxQty = config.maxQty || this.maxQty;
+            this.qty = ko.observable(this.initialQty);
+        },
+
+        decrease: function() {
+            let updateQty = parseInt(this.qty(), 10);
+            if (updateQty > 1) {
+                this.qty(updateQty - 1);
+            }
+        },
+
+        increase: function() {
+            let updateQty = parseInt(this.qty(), 10);
+            if (updateQty < this.maxQty) {
+                this.qty(updateQty + 1);
+            }
+        },
+
+        validateQty: function() {
+            let currentQty = parseInt(this.qty(), 10);
+            if (currentQty < 1) {
+                this.qty(1);
+            } else if (currentQty > this.maxQty) {
+                this.qty(this.maxQty);
+            }
+        }
+    });
+});
--- app/design/frontend/Magebit/Learning/requirejs-config.js
+++ app/design/frontend/Magebit/Learning/requirejs-config.js
@@ -5,21 +5,7 @@
  */
 
 var config = {
-    map: {
-        '*': {
-            'Magento_Catalog/css/styles': 'Magebit_Learning/css/styles',
-            qtyCounter: 'Magento_Catalog/js/qty-counter'
-        }
-    },
-    paths: {
-        'Magebit_Learning/css/styles': 'Magebit_Learning/css/styles',
-        qtyCounter: 'Magento_Catalog/js/qty-counter'
-    },
-    shim: {
-        'Magebit_Learning/js/view/product/view': {
-            deps: ['Magebit_Learning/css/styles']
-        }
-    }
+
 };
 
 require.config(config);
